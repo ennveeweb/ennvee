30b370f1298e1ad2a143b9d863b9c03ae4d74121
RaNa <ranganathan.prakaspathi@ennvee.net>
2026-08-12 17:56:05 +0530

    App JS

---
+++ my-app/app.js
@@ -0,0 +1,32 @@
+const next = require("next");
+
+const http = require("http");
+ 
+const port = process.env.PORT || 3000;
+ 
+const app = next({
+
+  dev: false,
+
+});
+ 
+const handle = app.getRequestHandler();
+ 
+app.prepare().then(() => {
+
+  http
+
+    .createServer((req, res) => {
+
+      handle(req, res);
+
+    })
+
+    .listen(port, "0.0.0.0", () => {
+
+      console.log(`Next.js application running on port ${port}`);
+
+    });
+
+});
+ 
